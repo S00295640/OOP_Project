03600b6ab5433ff6599442f88b059bb4292602f3
n12j1 <loirjulien27@gmail.com>
2026-02-18 15:14:01 +0100

    Transition Style

--- ProjectMcsr/ProjectMcsr/MainWindow.xaml.cs
+++ ProjectMcsr/ProjectMcsr/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -19,5 +20,29 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    private void BtnGuides_Click(object sender, RoutedEventArgs e)
+    {
+        DoubleAnimation slideOut = new DoubleAnimation()
+        {
+            To = -1000,
+            Duration = TimeSpan.FromSeconds(0.5),
+            AccelerationRatio = 0.5
+        };
+        TranslateTransform menuTransform = new TranslateTransform();
+        MainMenuPanel.RenderTransform = menuTransform;
+        menuTransform.BeginAnimation(TranslateTransform.XProperty, slideOut);
+
+        GuidePanel.Visibility = Visibility.Visible;
+        TimeSpan duration = TimeSpan.FromSeconds(0.8);
+        IEasingFunction ease = new CubicEase { EasingMode = EasingMode.EaseOut };
+        
+        DoubleAnimation slideIn = new DoubleAnimation(1000, 0, duration) { EasingFunction = ease };
+        GuidesTransform.BeginAnimation(TranslateTransform.XProperty, slideIn);
+    }
+
+    private void BtnBack_Click(object sender, RoutedEventArgs e)
+    {
+        //routine pour close le panel
     }
 }
--- ProjectMcsr/ProjectMcsr/obj/Debug/net8.0-windows/ProjectMcsr.AssemblyInfo.cs
+++ ProjectMcsr/ProjectMcsr/obj/Debug/net8.0-windows/ProjectMcsr.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectMcsr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+733537b9a5c70fc73ab72381953f2d5a69295bc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectMcsr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectMcsr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
