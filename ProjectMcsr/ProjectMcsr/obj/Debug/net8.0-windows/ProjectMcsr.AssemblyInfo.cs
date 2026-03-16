b15857731cf6db3ec89141f53bd3380d77722efd
S00295640 <S00295640@atu.ie>
2026-03-16 02:17:34 +0100

    Next steps

--- ProjectMcsr/ProjectMcsr/obj/Debug/net8.0-windows/ProjectMcsr.AssemblyInfo.cs
+++ ProjectMcsr/ProjectMcsr/obj/Debug/net8.0-windows/ProjectMcsr.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectMcsr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cec1dd9544b08455502f7bf52334c17aa175694c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80aeaf74fe04107fabe14100cd32a223d8882ffa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectMcsr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectMcsr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
